2068e4f7fefb03bf0fdb7a20356a90d1f804e51e
aldearco <albertodelarco@outlook.com>
2022-12-22 21:08:34 +0100

    Auth routes refactor

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Controllers\Auth\RegisterController;
+use Arco\Auth\Auth;
 use App\Models\User;
 use Arco\Http\Request;
-use Arco\Crypto\Hasher;
 use Arco\Http\Response;
 use Arco\Routing\Route;
+
+Auth::routes();
 
 Route::get("/", function () {
     if (isGuest()) {
@@ -19,31 +20,6 @@ Route::get("/form", fn () => Response::view("form"));
 Route::get("/user/{user}", fn (User $user) => json($user->toArray()));
 Route::get("/route/{param}", fn (int $param) => json(["param" => $param]));
 
-Route::get("/register", [RegisterController::class, "create"]);
-
-Route::post("/register", [RegisterController::class, "store"]);
-
-Route::get("/login", fn () => view("auth/login"));
-
-Route::post("/login", function (Request $request) {
-    $data = $request->validate([
-        "email" => ["required", "email"],
-        "password" => "required",
-    ]);
-
-    $user = User::firstWhere("email", $data["email"]);
-
-    if (is_null($user) || !app(Hasher::class)->verify($data["password"], $user->password)) {
-        return back()->withErrors([
-            "email" => ["email" => "Credentials do not match"]
-        ]);
-    }
-
-    $user->login();
-
-    return redirect("/");
-});
-
 Route::get("/note", function (Request $request) {
     if (isGuest()) {
         return Response::text("You need to be logged in");
@@ -54,8 +30,4 @@ Route::get("/note", function (Request $request) {
     ]);
 });
 
-Route::get("/logout", function () {
-    auth()->logout();
-    return redirect("/");
-});
 
--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -2,7 +2,10 @@
 
 namespace Arco\Auth;
 
+use Arco\Routing\Route;
+use App\Controllers\Auth\LoginController;
 use Arco\Auth\Authenticators\Authenticator;
+use App\Controllers\Auth\RegisterController;
 
 class Auth {
     public static function user(): ?Authenticatable {
@@ -11,5 +14,13 @@ class Auth {
 
     public static function isGuest(): bool {
         return is_null(self::user());
+    }
+
+    public static function routes() {
+        Route::get("/register", [RegisterController::class, "create"]);
+        Route::post("/register", [RegisterController::class, "store"]);
+        Route::get("/login", [LoginController::class, "create"]);
+        Route::post("/login", [LoginController::class, "store"]);
+        Route::get("/logout", [LoginController::class, "destroy"]);
     }
 }
