4542381ce631bd6cbb28bf0cad0f284f7b72dcd9
aldearco <albertodelarco@outlook.com>
2023-01-05 20:56:58 +0100

    Helpers if not exists function

--- src/Helpers/session.php
+++ src/Helpers/session.php
@@ -2,37 +2,43 @@
 
 use Arco\Session\Session;
 
-/**
- * Access to the client user's session
- *
- * @return Session
- */
-function session(): Session {
-    return app()->session;
-}
-
-/**
- * Return error stored if exist
- *
- * @param string $field
- */
-function error(string $field) {
-    $errors = session()->get("_errors", [])[$field] ?? [];
-
-    $keys = array_keys($errors);
-
-    if (count($keys) > 0) {
-        return $errors[$keys[0]];
+if (!function_exists('session')) {
+    /**
+     * Access to the client user's session
+     *
+     * @return Session
+     */
+    function session(): Session {
+        return app()->session;
     }
-
-    return null;
 }
 
-/**
- * Return the past request data values if exist
- *
- * @param string $field
- */
-function old(string $field) {
-    return session()->get("_old", [])[$field] ?? null;
+if (!function_exists('error')) {
+    /**
+     * Return error stored if exist
+     *
+     * @param string $field
+     */
+    function error(string $field) {
+        $errors = session()->get("_errors", [])[$field] ?? [];
+
+        $keys = array_keys($errors);
+
+        if (count($keys) > 0) {
+            return $errors[$keys[0]];
+        }
+
+        return null;
+    }
+}
+
+if (!function_exists('old')) {
+    /**
+     * Return the past request data values if exist
+     *
+     * @param string $field
+     */
+    function old(string $field) {
+        return session()->get("_old", [])[$field] ?? null;
+    }
 }
--- src/Helpers/string.php
+++ src/Helpers/string.php
@@ -1,58 +1,62 @@
 <?php
 
-/**
- * Turn any string into snake_case format
- *
- * @param string $str
- * @return string
- */
-function snake_case(string $str): string {
-    $snake_cased = [];
-    $skip = [' ', '-', '_', '/', '\\', '|', ',', '^', '\'', '"', '.', ';', ':', '*', '[', ']', '{', '}'];
+if (!function_exists('snake_case')) {
+    /**
+     * Turn any string into snake_case format
+     *
+     * @param string $str
+     * @return string
+     */
+    function snake_case(string $str): string {
+        $snake_cased = [];
+        $skip = [' ', '-', '_', '/', '\\', '|', ',', '^', '\'', '"', '.', ';', ':', '*', '[', ']', '{', '}'];
 
-    $i = 0;
+        $i = 0;
 
-    while ($i < strlen($str)) {
-        $last = count($snake_cased) > 0
-            ? $snake_cased[count($snake_cased) - 1]
-            : null;
-        $char = $str[$i++];
-        if (ctype_upper($char)) {
-            if ($last != '_') {
-                $snake_cased[] = '_';
-            }
-            $snake_cased[] = strtolower($char);
-        } elseif (ctype_lower($char)) {
-            $snake_cased[] = $char;
-        } elseif (in_array($char, $skip)) {
-            if ($last != '_') {
-                $snake_cased[] = '_';
-            }
-            while ($i < strlen($str) && in_array($str[$i], $skip)) {
-                $i++;
+        while ($i < strlen($str)) {
+            $last = count($snake_cased) > 0
+                ? $snake_cased[count($snake_cased) - 1]
+                : null;
+            $char = $str[$i++];
+            if (ctype_upper($char)) {
+                if ($last != '_') {
+                    $snake_cased[] = '_';
+                }
+                $snake_cased[] = strtolower($char);
+            } elseif (ctype_lower($char)) {
+                $snake_cased[] = $char;
+            } elseif (in_array($char, $skip)) {
+                if ($last != '_') {
+                    $snake_cased[] = '_';
+                }
+                while ($i < strlen($str) && in_array($str[$i], $skip)) {
+                    $i++;
+                }
             }
         }
-    }
 
-    if ($snake_cased[0] == '_') {
-        $snake_cased[0] = '';
-    }
+        if ($snake_cased[0] == '_') {
+            $snake_cased[0] = '';
+        }
 
-    if ($snake_cased[count($snake_cased) - 1] == '_') {
-        $snake_cased[count($snake_cased) - 1] = '';
-    }
+        if ($snake_cased[count($snake_cased) - 1] == '_') {
+            $snake_cased[count($snake_cased) - 1] = '';
+        }
 
-    return implode($snake_cased);
+        return implode($snake_cased);
+    }
 }
 
-/**
- * Get class basename and its returned as string
- *
- * @param string|object $class
- * @return string
- */
-function class_basename(string|object $class): string {
-    $class = is_object($class) ? get_class($class) : $class;
+if (!function_exists('class_basename')) {
+    /**
+     * Get class basename and its returned as string
+     *
+     * @param string|object $class
+     * @return string
+     */
+    function class_basename(string|object $class): string {
+        $class = is_object($class) ? get_class($class) : $class;
 
-    return basename(str_replace('\\', '/', $class));
+        return basename(str_replace('\\', '/', $class));
+    }
 }
