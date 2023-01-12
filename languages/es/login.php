527a44b446d25361beffe9d38830a743bac0ef25
Alberto del Arco <mktonline@behqsl.com>
2023-01-12 13:48:30 +0100

    Login Spanish translation file

---
+++ languages/es/login.php
@@ -0,0 +1,9 @@
+<?php 
+
+return [
+    'title' => 'Iniciar sesión',
+    'email' => 'Correo electrónico',
+    'password' => 'Contraseña',
+    'remember' => 'Recordar',
+    'submit' => 'Enviar',
+];
